9efe9de0bd3f1acd6313d51c1d0ff6e48175a74e
Kaitlyn <knmyers@ucsc.edu>
2020-04-19 21:47:45 -0700

    update

--- thermometer_obj.py
+++ thermometer_obj.py
@@ -15,78 +15,7 @@ R0 = 1000.0 # The resistance of the thermistor at 25C -change for different ther
 # Configure the Pi to use the BCM (Broadcom) pin names, rather than the pin positions
 GPIO.setmode(GPIO.BCM)
 
-# This project uses a thermistor, a component whose resistance varies with the temperature.
-# To measure its resistance, the code records the time it takes for a capacitor to fill  
-# when supplied by a current passing through the resistor. The lower the resistance the faster 
-# it fills up. 
-#
-# You can think of a capacitor as a tank of electricity, and as it fills with charge, the voltage
-# across it increases. We cannot measure that voltage directly, because the Raspberry Pi
-# does not have an analog to digital convertor (ADC or analog input). However, we can time how long it
-# takes for the capacitor to fill with charge to the extent that it gets above the 1.65V or so
-# that counts as being a high digital input. 
-# 
-# For more information on this technique take a look at: 
-# learn.adafruit.com/basic-resistor-sensor-reading-on-raspberry-pi
-# The code here is based on that in the Raspberry Pi Cookbook (Recipes 12.1 to 12.3)
 
-
-# Pin a charges the capacitor through a fixed 1k resistor and the thermistor in series
-# pin b discharges the capacitor through a fixed 1k resistor 
-
-class Thermometer:
-    def __init__(self, a_pin, b_pin):
-        self.a_pin = a_pin
-        self.b_pin = b_pin
-
-    def setup(self, input, out, on):
-        GPIO.setup(input, GPIO.IN)
-        GPIO.setup(out, GPIO.OUT)
-        GPIO.output(out, on)
-    # find something realtively close to the ambient temp
-# empty the capacitor ready to start filling it up
-    def discharge(self):
-        self.setup(self.a_pin, self.b_pin, False)
-        time.sleep(0.01)
-
-# return the time taken for the voltage on the capacitor to count as a digital input HIGH
-# than means around 1.65V
-    def charge_time(self):
-        self.setup(self.b_pin, self.a_pin, True)
-        t1 = time.time()
-        while not GPIO.input(self.b_pin):
-         pass
-        t2 = time.time()
-        return (t2 - t1) * 1000000 # microseconds
-
-# Take an analog reading as the time taken to charge after first discharging the capacitor
-    def analog_read(self):
-        self.discharge()
-        t = self.charge_time()
-        self.discharge()
-        return t
-
-# Convert the time taken to charge the cpacitor into a value of resistance
-# To reduce errors, do it lots of times and take the average.
-    def read_resistance(self):
-        n = 10
-        total = 0;
-        for i in range(0, n):
-            total = total + self.analog_read()
-        t = total / float(n)
-        T = t * 0.632 * 3.3
-        r = (T / C) - R1
-        return r
-
-
-    def read_temp_c(self):
-        R = self.read_resistance()
-        t0 = 273.15     # 0 deg C in K
-        t25 = t0 + 25.0 # 25 deg C in K
-    # Steinhart-Hart equation - Google it
-        inv_T = 1/t25 + 1/B * math.log(R/float(R0))
-        T = (1/inv_T - t0)
-        return T
 therm = Thermometer(17, 27)
 # group together all of the GUI code into a class called App
 class App:
